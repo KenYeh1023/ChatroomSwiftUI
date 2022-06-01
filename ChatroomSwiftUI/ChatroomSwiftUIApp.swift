4ff27df254b3780c25f8fede1c314b8510241588
KenYeh1023 <s1011451@gmail.com>
2022-06-01 10:20:53 +0800

    initial commit

---
+++ ChatroomSwiftUI/ChatroomSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatroomSwiftUIApp.swift
+//  ChatroomSwiftUI
+//
+//  Created by Ken Yeh on 2022/6/1.
+//
+
+import SwiftUI
+
+@main
+struct ChatroomSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChatroomSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChatroomSwiftUI
+//
+//  Created by Ken Yeh on 2022/6/1.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
