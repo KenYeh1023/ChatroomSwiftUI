be74ea4b069381c7d552f7236da087e2bb2be512
KenYeh1023 <s1011451@gmail.com>
2022-06-06 10:06:06 +0800

    Create Account Roughly UI

--- ChatroomSwiftUI/ContentView.swift
+++ ChatroomSwiftUI/ContentView.swift
@@ -10,17 +10,43 @@ import SwiftUI
 struct ContentView: View {
     
     @State var isLoginMode: Bool = false
+    @State var email = ""
+    @State var password = ""
     
     var body: some View {
         NavigationView {
             ScrollView {
                 Picker("picker view", selection: $isLoginMode) {
                     Text("Login")
+                        .tag(true)
                     Text("Create Account")
+                        .tag(false)
                 }.pickerStyle(SegmentedPickerStyle())
+                    .padding()
+                
+                Button(action: profileButtonPressed, label: {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 64))
+                        .padding()
+                })
+                
+                TextField("Email", text: $email)
+                TextField("Password", text: $password)
+                
+                Button(action: loginButtonPressed, label: {
+                    Text("Button")
+                })
             }
             .navigationTitle("Create Account")
         }
+    }
+    
+    func profileButtonPressed() {
+        print("Porfile")
+    }
+    
+    func loginButtonPressed() {
+        print("Login")
     }
 }
 
