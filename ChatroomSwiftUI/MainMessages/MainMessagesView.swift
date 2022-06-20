df62b87074e7dc63fcef6ba1ddd8748311d6acd6
KenYeh1023 <s1011451@gmail.com>
2022-06-20 10:26:43 +0800

    Initially wireframe Custom Navigation Bar

--- ChatroomSwiftUI/MainMessages/MainMessagesView.swift
+++ ChatroomSwiftUI/MainMessages/MainMessagesView.swift
@@ -10,22 +10,55 @@ import SwiftUI
 struct MainMessagesView: View {
     var body: some View {
         NavigationView {
-            ScrollView {
-                ForEach(0..<10, id: \.self) { num in
-                    HStack {
-                        Image(systemName: "person.fill")
-                            .font(.system(size: 32))
-                        VStack(alignment: .leading) {
-                            Text("User Name")
-                            Text("Hello from the other side")
+            VStack {
+                HStack(spacing: 16) {
+                    Image(systemName: "person.fill")
+                        .font(.system(size: 34, weight: .heavy))
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("Pickachu")
+                            .font(.system(size: 24, weight: .bold))
+                        HStack {
+                            Circle()
+                                .foregroundColor(.green)
+                                .frame(width: 12, height: 12)
+                            Text("Online")
+                                .font(.system(size: 12))
+                                .foregroundColor(Color(.lightGray))
                         }
-                        Spacer()
-                        Text("22d")
                     }
-                    Divider()
+                    Spacer()
+                    Image(systemName: "gear")
+                }
+                .padding()
+                
+                ScrollView {
+                    ForEach(0..<10, id: \.self) { num in
+                        VStack {
+                            HStack(spacing: 16) {
+                                Image(systemName: "person.fill")
+                                    .font(.system(size: 32))
+                                    .padding(8)
+                                    .overlay(RoundedRectangle(cornerRadius: 44)
+                                                .stroke(Color.black, lineWidth: 1)
+                                    )
+                                VStack(alignment: .leading) {
+                                    Text("User Name")
+                                        .font(.system(size: 16, weight: .bold))
+                                    Text("Hello from the other side")
+                                        .font(.system(size: 14))
+                                        .foregroundColor(Color(.lightGray))
+                                }
+                                Spacer()
+                                Text("22d")
+                                    .font(.system(size: 14, weight: .semibold))
+                            }
+                            Divider()
+                                .padding(.vertical, 8)
+                        }.padding(.horizontal)
+                    }
                 }
             }
-            .navigationTitle("Navigation Title")
+            .navigationBarHidden(true)
         }
     }
 }
